515905083b1bb269c94f5b6ca535be4155ff972e
SugiraVinc <sugiravincentg11@gmail.com>
2024-11-20 20:22:22 +0200

    new commits

---
+++ models/like.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const likeSchema = new mongoose.Schema({
+  contentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'ContributorContent', // Reference to your content model
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Reference to your user model
+    required: true,
+  },
+  like: {
+    type: Number,
+    default: 1, // Default value for a "like"
+  },
+}, { timestamps: true });
+
+// Do not add any unique index here
+const Like = mongoose.model('Like', likeSchema);
+export default Like;
